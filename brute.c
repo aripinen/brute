37c407f7c8ad134b4c6b89aae6b8b23fc9f20ea0
Alex <alex@alex-R425-R525.(none)>
2013-09-11 01:30:44 +0400

    brute prob

--- brute.c
+++ brute.c
@@ -9,7 +9,7 @@
 #include <sched.h>
 #include <sched.h>
 #define MAXLENGTH 30
-#define ALPHSTRING "csit"
+#define ALPHSTRING "abcdsefghijktz"
 typedef char result_t[MAXLENGTH];
 
 typedef enum run_mode_t{
@@ -23,8 +23,9 @@ typedef enum brute_mode_t{
 };
 
 typedef struct task_t{
-	char hash[20];
 	result_t pass;
+	int from;
+	int to;
 } task_t;
 
 typedef struct context_t{
@@ -32,6 +33,8 @@ typedef struct context_t{
 	char *hash;
 	int alphLength;
 	int length;
+	int countMassive[MAXLENGTH];
+	int complete;
 	result_t result;
 	enum run_mode_t run_mode;
 	enum brute_mode_t brute_mode;
@@ -48,41 +51,44 @@ queue_t queue;
 result_t password;
 struct crypt_data data_single;
 
-void queue_push(queue_t *queue,task_t *task){
-	sem_wait(&(queue->empty_sem));
-	pthread_mutex_lock(&(queue->tail_mutex));
-	queue->task[queue->tail]=*task;
+int *queue_push(task_t *task, context_t *context, struct crypt_data *data){
+	sem_wait(&(queue.empty_sem));
+	pthread_mutex_lock(&(queue.tail_mutex));
+	queue.task[queue.tail]=*task;
 	int i;
-	queue->tail++;
-	if (queue->tail==sizeof(queue->task)/sizeof(queue->task[0]))
-		queue->tail=0;
-	pthread_mutex_unlock(&(queue->tail_mutex));
-	sem_post(&(queue->full_sem));
+	queue.tail++;
+	if (queue.tail==sizeof(queue.task)/sizeof(queue.task[0]))
+		queue.tail=0;
+	pthread_mutex_unlock(&(queue.tail_mutex));
+	sem_post(&(queue.full_sem));
+	return 0;
 }
 
-void queue_init(queue_t *queue){
-	queue->tail=0;
-	queue->head=0;
-	pthread_mutex_init(&queue->tail_mutex,NULL);
-	pthread_mutex_init(&queue->head_mutex,NULL);
-	sem_init(&queue->full_sem,0,0);
-	sem_init(&queue->empty_sem,0,sizeof(queue->task)/sizeof(queue->task[0]));
+void queue_init(){
+	queue.tail=0;
+	queue.head=0;
+	pthread_mutex_init(&queue.tail_mutex,NULL);
+	pthread_mutex_init(&queue.head_mutex,NULL);
+	sem_init(&queue.full_sem,0,0);
+	sem_init(&queue.empty_sem,0,sizeof(queue.task)/sizeof(queue.task[0]));
 }
 
-task_t queue_pop(queue_t *queue){
+task_t queue_pop(context_t *context){
 	task_t task;
-	sem_wait(&(queue->full_sem));
-	pthread_mutex_lock(&(queue->head_mutex));
-	task=queue->task[queue->head];
+	sem_wait(&(queue.full_sem));
+	pthread_mutex_lock(&(queue.head_mutex));
+	task=queue.task[queue.head];
 	if (strcmp(task.pass,"")==0){
-		queue_push(queue,&task);
-	pthread_exit(0);
+		queue_push(&task, context, &data_single);
+		pthread_mutex_unlock(&(queue.head_mutex));
+		sem_post(&(queue.empty_sem));
+		pthread_exit(0);
 	}
-	queue->head++;
-	if (queue->head==sizeof(queue->task)/sizeof(queue->task[0]))
-		queue->head=0;
-	pthread_mutex_unlock(&(queue->head_mutex));
-	sem_post(&(queue->empty_sem));
+	queue.head++;
+	if (queue.head==sizeof(queue.task)/sizeof(queue.task[0]))
+		queue.head=0;
+	pthread_mutex_unlock(&(queue.head_mutex));
+	sem_post(&(queue.empty_sem));
 	return task;
 }
 
@@ -100,9 +106,12 @@ void process_args(int argc,char **argv,context_t *context){
 	context->hash=argv[optind];
 }
 
-int equelsHash(result_t *result, char *hash, struct crypt_data *data){
-	if (strcmp(hash, crypt_r(result,hash,data))==0)
+int *equelsHash(task_t *task, context_t *context, struct crypt_data *data){
+	if (strcmp(context->hash, crypt_r(task->pass, context->hash, data))==0)
+	{
+		context->complete = 1;
 		return 1;
+	}
 	else return 0;
 }
 
@@ -113,68 +122,49 @@ int check_run_mode(context_t *context){
 	}
 }
 
-void brute_rec(context_t *context, int count){
-	if (count<context->length){
+void brute_rec(context_t *context,task_t *task, int count, int (*prob)(struct task_t*, struct context_t*, struct crypt_data*)){
+	if (count<context->length && context->complete == 0){
 		int i;
 		for(i=0;i<context->alphLength;i++){
-			context->result[count]=context->alph[i];
-			if (count==context->length-1){
-				if (check_run_mode(context)==1){
-					task_t *task=(task_t*)malloc(sizeof(task_t));
-					if (strcmp(password,"")!=0){
-						queue_push(&queue,task);
-						pthread_exit(0);
-					}
-					memcpy(task->pass,context->result,context->length);
-					memcpy(task->hash,context->hash,20);
-					queue_push(&queue,task);
-				}
-				else{
-					if (equelsHash(context->result,context->hash,&data_single)){
-						memcpy(password,context->result,context->length);
-						return;
-					}
+			task->pass[count]=context->alph[i];
+			if (count==task->to - 1)
+			{
+				if ((int)prob(task, context, &data_single) == 1) 
+				{
+					memcpy(password, task->pass, context->length);
+					break;
 				}
 			}	
-			else brute_rec(context,count+1);
+			else brute_rec(context, task, count+1, prob);
 		}
   	}
 }
 
-void brute_iter(context_t *context, int *countMassive){
+void brute_iter(context_t *context, task_t *task, int (*prob)(struct task_t*, struct context_t*, struct crypt_data*)){
 	int i;
+	for(i = task->from; i < task->to; i++)
+		context->countMassive[i] = 0;
 	for(;;){
-		if (countMassive[0]>=context->alphLength){
-			i=0;
-			while (countMassive[i]>=context->alphLength-1){
-				if (i>context->length-2) return;
-					countMassive[i]=0;
-				context->result[i]=context->alph[0];
-				i++;
+		if (context->countMassive[task->to - 1] >= context->alphLength){
+			i = task->to-1;
+			while (context->countMassive[i] >= context->alphLength - 1){
+				if (i == task->from) return;
+				context->countMassive[i] = 0;
+				task->pass[i] = context->alph[0];
+				i--;
 			}
-			countMassive[i]++;
-			context->result[i]=context->alph[countMassive[i]];
+			context->countMassive[i]++;
+			task->pass[i] = context->alph[context->countMassive[i]];
 		}
 		else{
-			context->result[0]=context->alph[countMassive[0]];
-			countMassive[0]++;
-			context->result[context->length]='\0';
-			if (check_run_mode(context)==1){
-				task_t *task=(task_t*)malloc(sizeof(task_t));
-				if (strcmp(password,"")!=0){
-					queue_push(&queue,task);
-					break;
-				}
-				memcpy(task->pass,context->result,context->length);
-				memcpy(task->hash,context->hash,20);
-				queue_push(&queue,task);
+			task->pass[task->to-1]=context->alph[context->countMassive[task->to-1]];
+			context->countMassive[task->to-1]++;
+			if ((int) prob(task, context, &data_single) == 1) 
+			{
+				memcpy(password, task->pass, context->length+1);
+				return;
 			}
-			else{
-				if (equelsHash(context->result,context->hash,&data_single)){
-					memcpy(password,context->result,context->length);
-					break;
-				}
-			}	
+			if (context->complete == 1) return;
 		}
 	}
 }
@@ -183,76 +173,80 @@ void *thread_consumer(void *arg){
 	context_t *context=(context_t*) arg;
 	struct crypt_data data;
 	data.initialized=0;
-	int i;
+	int i,j;
+	task_t task;
 	while(1){
-		task_t task[5];
-		for (i=0;i<5;i++)
-			task[i]=queue_pop(&queue);
-		for (i=0;i<5;i++)
-			if(equelsHash(&task[i].pass,&task[i].hash,&data)){
-				memcpy(password,task[i].pass,context->length);
-			}
-	}
+		task=queue_pop(context);
+		task.from = task.to;
+		task.to = context->length;
+		brute_iter(context, &task, &equelsHash);
+	}	
 	pthread_exit(0);
 }
 
-void *thread_produsser(void *arg){
-	context_t *context=(context_t*) arg;
-	int j,countMassive[MAXLENGTH];
-	for (j=0;j<context->length;j++){
-		context->result[j]=context->alph[0];
-		countMassive[j]=0;
-	}
-	context->result[context->length]='\0';
-	switch(context->brute_mode){
-		case BM_REC:
-			brute_rec(context,0);
-			break;
-		case BM_ITER:
-			brute_iter(context,&countMassive);
-			break;
-	}
-	if (strcmp(password,"")==0){
-		task_t *task=(task_t*)malloc(sizeof(task_t));
-		queue_push(&queue,task);
-	}
-	pthread_exit(0);
+
+void thread_creater(context_t *context, int *threadIdCons)
+{
+	int i;
+	for(i = 0; i < sizeof(*threadIdCons) / sizeof(int); i++)
+		pthread_create(&threadIdCons[i], NULL, &thread_consumer, context);
+}
+
+void thread_closer (context_t *context, int *threadIdCons)
+{
+	int i;
+	task_t *final_task=(task_t*)malloc(sizeof(task_t));
+	memcpy(final_task->pass,"",1);
+	queue_push(final_task, context, &data_single);
+	for (i=0;i<sizeof(*threadIdCons) / sizeof(int);i++)
+		pthread_join(threadIdCons[i],NULL);
 }
 
 int main(int argc, char *argv[]){
 	data_single.initialized=0;
+	task_t task;
+	task.from = 0;
 	context_t context;
 	context.alph=ALPHSTRING;
 	context.alphLength=strlen(context.alph);
 	context.length=4;
-	int i,count,nProcess,threadIdProd,countMassive[MAXLENGTH],threadIdCons[5],check=1;
-	nProcess=sysconf(_SC_NPROCESSORS_CONF);
-	queue_init(&queue);
+	context.complete=0;
+	int j,i,count,nProcess = sysconf(_SC_NPROCESSORS_CONF), *threadIdCons = (int*)malloc(sizeof(int) * nProcess);
+	for (j = 0; j < context.length; j++){
+		context.result[j] = context.alph[0];
+		context.countMassive[j] = 0;
+	}
+	context.result[context.length] = '\0';
+	memcpy(task.pass, context.result, context.length+1);
+	queue_init();
 	process_args(argc,argv,&context);
-	if (check_run_mode(&context)==1){
-		pthread_create(&threadIdProd,NULL,&thread_produsser,&context);
-		for (i=0;i<nProcess;i++)
-			pthread_create(&threadIdCons[i],NULL,&thread_consumer,&context);
-		pthread_join(threadIdProd,NULL);
-		for (i=0;i<nProcess;i++)
-			pthread_join(&threadIdCons[i],NULL);
+	if (check_run_mode(&context) == 1)
+	{
+		task.to = context.length - 2;
+		thread_creater(&context, threadIdCons);
 	}
-	else{
-		int j,countMassive[MAXLENGTH];
-		for (j=0;j<context.length;j++){
-			context.result[j]=context.alph[0];
-			countMassive[j]=0;
-		}
-		switch(context.brute_mode){
-			case BM_REC:
-				brute_rec(&context,0);
-				break;
-			case BM_ITER:
-				brute_iter(&context,&countMassive);
-				break;
-		}
+	else task.to = context.length;
+	switch(context.brute_mode){
+		case BM_REC:
+			if (check_run_mode(&context) == 1)
+			{
+				brute_rec(&context, &task, 0, &queue_push);
+				thread_closer(&context, threadIdCons);
+			}
+			else 
+				brute_rec(&context, &task, 0, &equelsHash);
+			break;
+		case BM_ITER:
+			if (check_run_mode(&context) == 1)
+			{
+				brute_iter(&context, &task, &queue_push);
+				thread_closer(&context, threadIdCons);
+			}
+			else 
+				brute_iter(&context, &task, &equelsHash);
+			break;
 	}
-	if (strcmp(password,"")==0)
+	if (context.complete == 0)
 		printf("password not found\n");
 	else
   		printf("pass %s\n",password);
